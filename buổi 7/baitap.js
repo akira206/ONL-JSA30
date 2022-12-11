1d1ad9c73af2caced57cbff9905aa221b0e8225b
akira2kar6 <109467738+akira2kar6@users.noreply.github.com>
2022-12-11 21:40:13 +0700

    buoi 7

--- buổi 7/baitap.js
+++ buổi 7/baitap.js
@@ -1,27 +1,24 @@
 // bài 1
-// function student = (name, age, address)
-
-
-
-
-
-
-
-
-
-
-
-
+// function student = (name, age, address) 
+function student() {
+    const student = {
+        name : 'đạt',
+        age : '16',
+        address : 'Vĩnh Phúc'
+    };
+    console.log(student);
+  }
+student();
 // bài 2
 var smartPhones = [ 
     { name: 'iphone', price: '649' }, 
     { name: 'Galaxy S6', price: '576' }, 
     { name: 'Galaxy Note 5', price: '489' }  
 ];
-console.log(smartPhones)
-
-
-
+console.log;smartPhones.map(function(smartPhone) {
+    return smartPhone.price;
+});
+console.log(smartPhones.map(smartPhone => smartPhone.price));
 // bài 3
 var foo = () => {
     console.log('x,y,z');
@@ -32,3 +29,11 @@ var foo = () => {
     alert('hello');
   };
   foo();
+
+var foo = (a,b) => {
+    let m = a+b*100
+    return m
+};
+foo(1,3)
+console.log('hàm ko có return',foo('hello'));
+console.log('hàm có return',foo(1,3));  
